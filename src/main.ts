64ec0dc8a70526dd5748019d4ae43db63d26f8fd
cherry <1044369926@qq.com>
2022-07-04 14:33:56 +0800

    feat:render函数真正实现

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,19 @@
-import { createApp } from "vue";
-import App from "./App.vue";
+import { createApp } from 'vue'
+import App from './App.vue'
+// import { createApp, defineComponent, h } from 'vue'
+// import HelloWorldVue from './components/HelloWorld.vue'
+// // eslint-disable-next-line @typescript-eslint/no-var-requires
+// const Img = require('./assets/logo.png')
 
-createApp(App).mount("#app");
+// const App = defineComponent({
+//   render() {
+//     return h('div', { id: '#app' }, [
+//       h('img', { alt: 'Vue logo', src: Img }),
+//       h(HelloWorldVue, {
+//         msg: 'Welcome to Your Vue.js + TypeScript App',
+//         age: 18,
+//       }),
+//     ])
+//   },
+// })
+createApp(App).mount('#app')
